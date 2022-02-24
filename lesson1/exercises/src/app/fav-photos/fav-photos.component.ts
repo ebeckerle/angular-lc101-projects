9a7f34480f7ab366464808d3d3b1e62bddd78548
Elizabeth <elzbthyorke@gmail.com>
2022-02-24 16:38:08 -0600

    1st commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'This is one of my Favorite Photos';
+  image1 = 'https://www1.nyc.gov/assets/wildlifenyc/images/content/iStock-509211079.jpg';
+  image2 = 'https://www.woodlandtrust.org.uk/media/1396/fox-stalking-alamy-dew6rp-age-fotostock.jpg?center=0.52,0.59909909909909909&mode=crop&heightratio=0.5622047244094488188976377953&width=647&rnd=132078467430000000http://www.shutterstock.com/gallery-265342p1.html';
+  image3 = 'https://www.macleans.ca/wp-content/uploads/2020/06/FOXES-DHOPADE-JUNE29-01-766x575.jpg';
 
   constructor() { }
 
